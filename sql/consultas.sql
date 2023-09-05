1eba7a43e36a1c98675eac2cbd343d36a387e7a6
Feez-E <142937020+Feez-E@users.noreply.github.com>
2023-09-05 20:59:06 -0300

    updated database

---
+++ sql/consultas.sql
@@ -0,0 +1,30 @@
+USE ViaUY;
+
+SELECT * FROM Usuario;
+SELECT * FROM Linea;
+SELECT * FROM Transita;
+SELECT * FROM Asiento;
+SELECT * FROM Reserva;
+
+SELECT valorDouble FROM ParametroDouble WHERE nombre_Parametro = "precioKm"; -- Devuelve el valor del kilometro en base a su parametro.
+SELECT distancia FROM Tramo WHERE idInicial = 1 AND idFinal = 2; -- Devuelve el largo de un tramo, la distancia entre una parada y otra
+SELECT multiplicador FROM Tramo INNER JOIN Estado ON Tramo.estado_Estado = Estado.estado WHERE idInicial = 1 AND idFinal = 2; -- Devuelve el multiplicador asociado al estado de un tramo
+SELECT ROUND(SUM(multiplicador)-(COUNT(*)-1), 2) FROM Caracteristica WHERE numero_Unidad = 2; -- Devuelve el multiplicador general de todas las caracteristicas de una unidad, redondeandolo.
+
+SELECT ROUND((SELECT distancia FROM Tramo WHERE idInicial = 1 AND idFinal = 2)*
+(SELECT multiplicador FROM Tramo INNER JOIN Estado ON Tramo.estado_Estado = Estado.estado WHERE idInicial = 3 AND idFinal = 4)*
+(SELECT valorDouble FROM ParametroDouble WHERE nombre_Parametro = "precioKm")*
+(SELECT ROUND(SUM(multiplicador)-(COUNT(*)-1), 2) FROM Caracteristica WHERE numero_Unidad = 2), 2); -- Calcula el precio de un tramo en base a las caracteristicas de la Unidad
+
+SELECT * FROM Usuario;
+
+SELECT DISTINCT(apodo), direccion, codigo_Tiquet, numero_Asiento, fecha, horaSalida_S_T_Asiento, horaLlegada_L_T_Asiento, fechaLimite FROM Parada
+INNER JOIN Usuario ON Parada.id = Usuario.id
+INNER JOIN Reserva ON Usuario.id = Reserva.id_Usuario
+WHERE Usuario.id = "1";
+
+
+SELECT *
+FROM Linea
+INNER JOIN Transita ON Linea.codigo = Transita.codigo_L_Recorre;
+
--- sql/schema.sql
+++ sql/schema.sql
@@ -11,7 +11,7 @@ vigencia BOOLEAN
 
 CREATE TABLE Estado (
 estado ENUM('R','M','N','B','E') PRIMARY KEY,
-multiplicador FLOAT NOT NULL
+multiplicador DOUBLE NOT NULL
 );
 
 CREATE TABLE Tramo(
@@ -20,7 +20,7 @@ FOREIGN KEY(idInicial) REFERENCES Parada(id),
 idFinal INT,
 FOREIGN KEY(idFinal) REFERENCES Parada(id),
 tiempo TIME NOT NULL,
-distancia FLOAT NOT NULL, 	
+distancia DOUBLE NOT NULL, 	
 estado_Estado ENUM('R','M','N','B','E'),
 FOREIGN KEY(estado_Estado) REFERENCES Estado(estado),
 PRIMARY KEY (idInicial, idFinal)
@@ -47,8 +47,8 @@ idFinal_Tramo INT,
 FOREIGN KEY(idInicial_Tramo, idFinal_Tramo) REFERENCES Tramo(idInicial, idFinal),
 codigo_Linea INT,
 FOREIGN KEY(codigo_Linea) REFERENCES Linea(codigo),
-orden INT NOT NULL,
-PRIMARY KEY (idInicial_Tramo, idFinal_Tramo, codigo_Linea)
+orden INT,
+PRIMARY KEY (idInicial_Tramo, idFinal_Tramo, codigo_Linea, orden)
 );
 
 CREATE TABLE Unidad(
@@ -64,27 +64,31 @@ CREATE TABLE Caracteristica(
 numero_Unidad INT,
 FOREIGN KEY(numero_Unidad) REFERENCES Unidad(numero),
 propiedad VARCHAR(30),
-multiplicador INT NOT NULL,
+multiplicador DOUBLE NOT NULL,
 PRIMARY KEY(numero_Unidad, propiedad)
 );
 
-CREATE TABLE Horario(
-horaSalida TIME NOT NULL,
-horaLlegada TIME NOT NULL,
-PRIMARY KEY(horaSalida, horaLlegada)
+CREATE TABLE Salida(
+horaSalida TIME PRIMARY KEY
+);
+
+CREATE TABLE Llegada(
+horaLlegada TIME PRIMARY KEY
 );
 
 CREATE TABLE Transita(
 idInicial_T_Recorre INT,
 idFinal_T_Recorre INT,
 codigo_L_Recorre INT,
-FOREIGN KEY(idInicial_T_Recorre, idFinal_T_Recorre, codigo_L_Recorre) REFERENCES Recorre (idInicial_Tramo, idFinal_Tramo, codigo_Linea),
+orden_Recorre INT,
+FOREIGN KEY(idInicial_T_Recorre, idFinal_T_Recorre, codigo_L_Recorre, orden_Recorre) REFERENCES Recorre (idInicial_Tramo, idFinal_Tramo, codigo_Linea, orden),
 numero_Unidad INT,
 FOREIGN KEY(numero_Unidad) REFERENCES Unidad (numero),
-horaSalida_Horario TIME,
-horaLlegada_Horario TIME,
-FOREIGN KEY(horaSalida_Horario, horaLlegada_Horario) REFERENCES Horario (horaSalida, horaLlegada),
-PRIMARY KEY(idInicial_T_Recorre, idFinal_T_Recorre, codigo_L_Recorre, numero_Unidad, horaSalida_Horario, horaLlegada_Horario)
+horaSalida_Salida TIME,
+FOREIGN KEY(horaSalida_Salida) REFERENCES Salida (horaSalida),
+horaLlegada_Llegada TIME,
+FOREIGN KEY(horaLlegada_Llegada) REFERENCES Llegada (horaLlegada),
+PRIMARY KEY(idInicial_T_Recorre, idFinal_T_Recorre, codigo_L_Recorre, orden_Recorre, numero_Unidad, horaSalida_Salida, horaLlegada_Llegada)
 );
 
 CREATE TABLE Parametro(
@@ -92,48 +96,47 @@ nombre VARCHAR(20) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE ParametroBoolean(
-nombre_Parametro VARCHAR(20) PRIMARY KEY NOT NULL,
+nombre_Parametro VARCHAR(20) PRIMARY KEY,
 FOREIGN KEY(nombre_Parametro) REFERENCES Parametro (nombre),
-valorBoolean BOOLEAN
+valorBoolean BOOLEAN NOT NULL
 );
 
-CREATE TABLE ParametroInt(
-nombre_Parametro VARCHAR(20) PRIMARY KEY NOT NULL,
+CREATE TABLE ParametroDouble(
+nombre_Parametro VARCHAR(20) PRIMARY KEY,
 FOREIGN KEY(nombre_Parametro) REFERENCES Parametro (nombre),
-valorInt INT
+valorDouble DOUBLE NOT NULL
 );
 
 CREATE TABLE ParametroVarchar(
-nombre_Parametro VARCHAR(20) PRIMARY KEY NOT NULL,
+nombre_Parametro VARCHAR(20) PRIMARY KEY,
 FOREIGN KEY(nombre_Parametro) REFERENCES Parametro (nombre),
-valorVarchar VARCHAR(50)
+valorVarchar VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE ParametroTime(
-nombre_Parametro VARCHAR(20) PRIMARY KEY NOT NULL,
+nombre_Parametro VARCHAR(20) PRIMARY KEY,
 FOREIGN KEY(nombre_Parametro) REFERENCES Parametro (nombre),
-valorTime TIME
+valorTime TIME NOT NULL
 );
 
 CREATE TABLE Asiento(
-numero INT NOT NULL,
+numero INT,
 idInicial_T_R_Transita INT,
 idFinal_T_R_Transita INT,
 codigo_L_R_Transita INT,
+orden_R_Transita INT,
 numero_U_Transita INT,
-FOREIGN KEY(idInicial_T_R_Transita, idFinal_T_R_Transita, codigo_L_R_Transita, numero_U_Transita) REFERENCES Transita (idInicial_T_Recorre, idFinal_T_Recorre, codigo_L_Recorre, numero_Unidad),
-horaSalida_H_Transita TIME,
-horaLlegada_H_Transita TIME,
-FOREIGN KEY(horaSalida_H_Transita, horaLlegada_H_Transita) REFERENCES Transita (horaSalida_Horario, horaLlegada_Horario),
-nombre_P_ParametroInt VARCHAR(30),
-FOREIGN KEY(nombre_P_ParametroInt) REFERENCES ParametroInt (nombre_Parametro),
-precio INT NOT NULL,
-disponibilidad BOOLEAN,
-PRIMARY KEY(numero, idInicial_T_R_Transita, idFinal_T_R_Transita, codigo_L_R_Transita, numero_U_Transita, horaSalida_H_Transita, horaLlegada_H_Transita)
+horaSalida_S_Transita TIME,
+horaLlegada_L_Transita TIME,
+FOREIGN KEY(idInicial_T_R_Transita, idFinal_T_R_Transita, codigo_L_R_Transita, orden_R_Transita, numero_U_Transita, horaSalida_S_Transita, horaLlegada_L_Transita) REFERENCES Transita (idInicial_T_Recorre, idFinal_T_Recorre, codigo_L_Recorre, orden_Recorre, numero_Unidad, horaSalida_Salida, horaLlegada_Llegada),
+nombre_P_ParametroDouble VARCHAR(30),
+FOREIGN KEY(nombre_P_ParametroDouble) REFERENCES ParametroDouble (nombre_Parametro),
+precio DOUBLE NOT NULL,
+PRIMARY KEY(numero, idInicial_T_R_Transita, idFinal_T_R_Transita, codigo_L_R_Transita, orden_R_Transita, numero_U_Transita, horaSalida_S_Transita, horaLlegada_L_Transita)
 );
 
 CREATE TABLE Permiso (
-nombre VARCHAR(25) PRIMARY KEY NOT NULL,
+nombre VARCHAR(40) PRIMARY KEY NOT NULL,
 descripcion VARCHAR(100) NOT NULL,
 url VARCHAR(50)
 );
@@ -148,6 +151,7 @@ nombre_Rol VARCHAR(20),
 FOREIGN KEY(nombre_Rol) REFERENCES Rol (nombre),
 nombre_Permiso VARCHAR(40),
 FOREIGN KEY(nombre_Permiso) REFERENCES Permiso (nombre),
+FOREIGN KEY(nombre_Permiso) REFERENCES Permiso (nombre),
 PRIMARY KEY(nombre_Rol, nombre_Permiso)
 );
 
@@ -157,7 +161,7 @@ apodo VARCHAR(32) UNIQUE KEY NOT NULL,
 nombre VARCHAR(20) NOT NULL,
 apellido VARCHAR(25) NOT NULL,
 correo VARCHAR(64) NOT NULL,
-contrasena VARCHAR(64) NOT NULL,
+contrasena VARCHAR(60) NOT NULL,
 telefono VARCHAR(10) NOT NULL,
 fechaNac DATE NOT NULL,
 nombre_Rol VARCHAR(20),
@@ -169,7 +173,7 @@ metodo VARCHAR(25) PRIMARY KEY
 );
 
 CREATE TABLE Tiquet (
-codigo INT PRIMARY KEY
+codigo VARCHAR(16) PRIMARY KEY
 );
 
 CREATE TABLE Reserva(
@@ -177,11 +181,11 @@ numero_Asiento INT,
 idInicial_T_R_T_Asiento INT,
 idFinal_T_R_T_Asiento INT,
 codigo_L_R_T_Asiento INT,
+orden_R_T_Asiento INT,
 numero_U_T_Asiento INT,
-FOREIGN KEY(numero_Asiento, idInicial_T_R_T_Asiento, idFinal_T_R_T_Asiento, codigo_L_R_T_Asiento, numero_U_T_Asiento) REFERENCES Asiento (numero, idInicial_T_R_Transita, idFinal_T_R_Transita, codigo_L_R_Transita, numero_U_Transita),
-horaSalida_H_T_Asiento TIME,
-horaLlegada_H_T_Asiento TIME,
-FOREIGN KEY(horaSalida_H_T_Asiento, horaLlegada_H_T_Asiento) REFERENCES Asiento (horaSalida_H_Transita, horaLlegada_H_Transita),
+horaSalida_S_T_Asiento TIME,
+horaLlegada_L_T_Asiento TIME,
+FOREIGN KEY(numero_Asiento, idInicial_T_R_T_Asiento, idFinal_T_R_T_Asiento, codigo_L_R_T_Asiento, orden_R_T_Asiento, numero_U_T_Asiento, horaSalida_S_T_Asiento, horaLlegada_L_T_Asiento) REFERENCES Asiento (numero, idInicial_T_R_Transita, idFinal_T_R_Transita, codigo_L_R_Transita, orden_R_Transita, numero_U_Transita, horaSalida_S_Transita, horaLlegada_L_Transita),
 id_Usuario INT,
 FOREIGN KEY(id_Usuario) REFERENCES Usuario(id),
 nombre_P_ParametroTime VARCHAR(20),
@@ -189,10 +193,10 @@ FOREIGN KEY(nombre_P_ParametroTime) REFERENCES ParametroTime(nombre_Parametro),
 fechaLimite DATE,
 metodo_MetodoPago VARCHAR(25),
 FOREIGN KEY(metodo_MetodoPago) REFERENCES MetodoPago(metodo),
-estado ENUM("Pago", "En espera", "Cancelado", "Anulado"),
-fecha DATE,
-hora TIME,
-codigo_Tiquet INT,
+estado ENUM("Pagado", "En espera", "Cancelado", "Anulado"),
+fecha DATE NOT NULL,
+hora TIME NOT NULL,
+codigo_Tiquet VARCHAR(16),
 FOREIGN KEY(codigo_Tiquet) REFERENCES Tiquet(codigo),
-PRIMARY KEY(codigo_Tiquet, numero_Asiento, idInicial_T_R_T_Asiento, idFinal_T_R_T_Asiento, codigo_L_R_T_Asiento, numero_U_T_Asiento, horaSalida_H_T_Asiento, horaLlegada_H_T_Asiento)
+PRIMARY KEY(codigo_Tiquet, numero_Asiento, idInicial_T_R_T_Asiento, idFinal_T_R_T_Asiento, codigo_L_R_T_Asiento, orden_R_T_Asiento, numero_U_T_Asiento, horaSalida_S_T_Asiento, horaLlegada_L_T_Asiento)
 );
